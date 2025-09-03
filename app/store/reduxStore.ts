86b6d039216a7b0772782c0a3ce9e982f71d141a
Ashish Shah <eiepixxstuff@gmail.com>
2025-09-03 06:04:14 +0530

    setup done redux store.

---
+++ app/store/reduxStore.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { articleApi } from "../services/articleApi";
+
+
+export const store = configureStore({
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware),
+    devTools: process.env.NODE_ENV !== "production",
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
